8b4b68be835f03b6fc8e671b000461a9c1d63f4c
Vanishree1 <vani.yaji94@gmail.com>
2018-04-15 21:15:20 +0530

    Remoevd globally registered components

--- src/main.js
+++ src/main.js
@@ -13,23 +13,13 @@ import 'vue-awesome/icons/spinner'
 import 'vue-awesome/icons/check-circle-o'
 import 'vue-awesome/icons/book'
 import Icon from 'vue-awesome/components/Icon'
-import SplashScreen from './components/SplashScreen'
-import LoginScreen from './components/LoginScreen'
-import PageNotFound from './components/PageNotFound'
-import PreloadingImage from './directives/PreLoadingImage'
-// import { VLazyImagePlugin } from 'v-lazy-image'
 
 Vue.config.productionTip = false
 Vue.component('Icon', Icon)
-Vue.component('SplashScreen', SplashScreen)
-Vue.component('LoginScreen', LoginScreen)
-Vue.component('PageNotFound', PageNotFound)
-Vue.directive('image', PreloadingImage)
-// Vue.use(VLazyImagePlugin)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App, Icon, SplashScreen, LoginScreen, PageNotFound },
+  components: { App, Icon },
   template: '<App/>'
 })
